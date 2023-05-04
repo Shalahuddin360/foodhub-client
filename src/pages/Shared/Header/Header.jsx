7a5fb3ac2bc126069e02714782913b6014a9cffa
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-05-04 07:29:58 +0600

    5th commit loaded data from Food Hub server site and displayed each chef's photo , chef name , chef description , year of experience ,number of recipes and like card

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,43 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 const Header = () => {
-    return (
-        <Container className='mt-4'>
-            <div className='d-flex align-items-center'>
-                <Button variant='danger'>Latest</Button>
-                <Marquee className='text-danger' pauseOnHover={true} speed={50}>
-                    I can be a React component, multiple React components, or just some text............... I can be a React component, multiple React components, or just some text.
-                </Marquee>
-            </div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  const { user } = useContext(AuthContext)
+  return (
+    <Container className='mt-4'>
+      <div className='d-flex align-items-center'>
+        <Button variant='danger'>Latest</Button>
+        <Marquee className='text-danger' pauseOnHover={true} speed={50}>
+          I can be a React component, multiple React components, or just some text............... I can be a React component, multiple React components, or just some text.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 
-      <Container>
-        <Navbar.Brand href="#home">FoodHub</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Blog</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">
-            <Button variant="primary">Profile</Button>
-            </Nav.Link>
+        <Container>
+          <Navbar.Brand href="#home">FoodHub</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+                <Link to="/">Home</Link>
+             
 
-            <Nav.Link href="#deets">
-            <Button variant="primary">Login</Button>
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-            <Button variant="primary">Registration</Button>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
+              <Nav.Link href="#pricing">Blog</Nav.Link>
+            </Nav>
+         
+            <Nav>
+              {
+                user && 
+
+                  <FaUserCircle style={{ fontSize: '2rem' }} />
+             
+                 }
+  
+                {user ?
+                  <Button variant="primary">Log Out</Button> :
+                  <Link to="/login"><Button variant="primary">Login</Button></Link>
+
+                }
+        
+
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="primary">Registration</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-    );
+      </Navbar>
+
+    </Container>
+  );
 };
 
 export default Header;
